09d54544922aad31f8a42cd1133a090c5dfc54e9
isenberg <8257002+isenberg@users.noreply.github.com>
2023-04-19 19:39:20 -0700

    Add files via upload

---
+++ AppExample.java
@@ -0,0 +1,66 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import java.net.*;
+import javax.imageio.*;
+import javax.swing.*;
+
+public class AppExample {
+    public static void main(String avg[]) throws IOException
+    {
+        String appName = "AppExample";
+        String imgFilename = "marsduke.png";
+        JLabel textLabel = new JLabel("<html>Spirit Mars Rover, Pancam, "
+            + "Sol 120, 2004-05-05, image ID 2P137011382EFF4000P2563L5M1</html>");
+        String links[] = {
+            "https://areo.info/mer/spirit/120",
+            "https://mars.nasa.gov/mer/gallery/all/spirit_p120.html"
+        };
+        JLabel linkLabels[] = {
+            new JLabel("<html>Color image: <a href='"
+                + links[0] + "'>" + links[0] + "</a></html>"),
+            new JLabel("<html>Raw image, NASA / JPL-Caltech: <a href='"
+                + links[1] + "'>" + links[1] + "</a></html>")
+        };
+        int space = 10;
+
+        String appDir = AppExample.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        if (appDir.endsWith(".jar"))
+        {
+            appDir = new File(appDir).getParent();
+        }
+        BufferedImage img = ImageIO.read(new File(appDir, imgFilename));
+
+        JFrame frame=new JFrame(appName);
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBorder(BorderFactory.createEmptyBorder(space, space, space, space));
+        ImageIcon icon=new ImageIcon(img);
+        JLabel iconLabel=new JLabel();
+        iconLabel.setIcon(icon);
+        panel.add(iconLabel);
+        panel.add(Box.createRigidArea(new Dimension(0, space)));
+        panel.add(textLabel);
+        for (int i=0; i<2; i++) {
+            String link = links[i];
+            linkLabels[i].setCursor(new Cursor(Cursor.HAND_CURSOR));
+            linkLabels[i].addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent ev) {
+                    try {
+                        Desktop.getDesktop().browse(new URI(link));
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            });
+            panel.add(linkLabels[i]);
+        }
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
---
+++ makeitso.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+JAVA_HOME=$(/usr/libexec/java_home -v 17)
+BUILDDIR=tmpbuild
+ICONDIR=app.iconset
+
+# re-using the image file also for the app icon
+mkdir -p $ICONDIR
+rprev=""
+for r in 1024 512 256 128 64 32 16; do
+  sips -z $r $r marsduke.png --out $ICONDIR/icon_${r}x${r}.png
+  if [ "$rprev" ]; then
+    cp $ICONDIR/icon_${rprev}x${rprev}.png $ICONDIR/icon_${r}x${r}@2x.png
+  fi
+  rprev=$r
+done
+rm $ICONDIR/icon_1024x1024.png
+iconutil -c icns $ICONDIR 
+
+mkdir -p $BUILDDIR
+cp marsduke.png $BUILDDIR/
+
+$JAVA_HOME/bin/javac AppExample.java
+$JAVA_HOME/bin/jar -cvfe $BUILDDIR/app.jar AppExample AppExample*.class
+
+# create .app bundle
+# or use --type dmg instead of --type app-image
+# to create .app bundle inside a .dmg image
+$JAVA_HOME/bin/jpackage --type app-image \
+ --dest . --input $BUILDDIR --icon app.icns --name AppExample \
+ --main-class AppExample --main-jar app.jar --verbose
+
+rm -rf $BUILDDIR $ICONDIR
+rm app.icns AppExample*.class
